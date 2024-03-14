91aa14c2e956cf1ebb3949e7ca0faf36ee195e76
filipemilk <filipeleche@gmail.com>
2024-03-14 14:46:07 -0300

    UpperCase

--- UpperCase/UperCase.js
+++ UpperCase/UperCase.js
@@ -1,13 +1,19 @@
-var frase = 'ana paula de olIveira sANTOS'
+//var frase = 'ana paula de olIveira sANTOS'
 
-var palavra = frase.split(' ')
+var nome = document.getElementById('inome')
+var butao = document.getElementById('but')
 
-for (let i in palavra) {
-    if (palavra[i].length > 2) {
-        palavra[i] = palavra[i].charAt(0).toUpperCase() + palavra[i].slice(1).toLowerCase()
-    } else {
-        palavra[i] = palavra[i].toLowerCase()
+
+function letraMaiuscula() {
+    var palavra = nome.value.split(' ')
+
+    for (let i in palavra) {
+        if (palavra[i].length > 2) {
+            palavra[i] = palavra[i].charAt(0).toUpperCase() + palavra[i].slice(1).toLowerCase()
+        } else {
+            palavra[i] = palavra[i].toLowerCase()
+        }
     }
+    window.alert(palavra.join(' '))
 }
-console.log(palavra.join(' '))
 
