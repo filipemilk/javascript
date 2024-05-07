0f31df1d261536f5cca03cfe22602d9d10d5a8a6
filipemilk <filipeleche@gmail.com>
2024-05-06 22:23:06 -0300

    manifest

---
+++ Bet365/index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+function init(){
+    const placar = document.querySelector('div.ovm-Fixture_Container > div.ovm-MarketGroup > div > div.ovm-AlternativeMarketHeader').innerText
+}
+
+init()
