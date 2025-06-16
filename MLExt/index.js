f4873a63393833068d8922476f7949e03803e09f
filipemilk <filipeleche@gmail.com>
2025-06-16 13:57:23 -0300

    Token Atualizado

--- MLExt/index.js
+++ MLExt/index.js
@@ -36,7 +36,7 @@ function formatMoney(value) {
 async function handleMlApi(url) {
     try {
         //ACCESS TOKEN FOI DA OUTRA AULA
-        const access_token = 'APP_USR-5295873722472032-061611-e68d609ca19a2165a8cbe18caa28c54d-154458559'
+        const access_token = 'APP_USR-5295873722472032-061612-2141fc3ded387663336f3b758c019a0d-154458559'
 
         const config = {
             method: 'GET',
