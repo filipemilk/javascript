c83b4cd7ba95b3a17c071785df7dbec5e6c927e3
filipemilk <filipeleche@gmail.com>
2025-06-28 15:05:57 -0300

    Prorrogação

--- Bet365/index.js
+++ Bet365/index.js
@@ -5,7 +5,7 @@ function init(){
 
     for (let i = 0; i < elementos.length; i++) {
         let elemento = elementos[i].innerText
-        if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Para Marcar o 7° Gol") {
+        if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Resultado da Prorrogação") {
             console.log(elemento)
             alert(elemento)
         } else {
