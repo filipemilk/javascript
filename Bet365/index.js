94d656d91b6448662e61520e109daac24dad5016
filipemilk <filipeleche@gmail.com>
2025-06-29 12:59:36 -0300

    loop

--- Bet365/index.js
+++ Bet365/index.js
@@ -1,17 +1,28 @@
 "use strict"
 
 function init(){
-    const elementos = document.querySelectorAll(".ovm-AlternativeMarketHeader")
-
-    for (let i = 0; i < elementos.length; i++) {
-        let elemento = elementos[i].innerText
-        if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Resultado da Prorrogação") {
+    
+    function penalti() {
+        const elementos = document.querySelectorAll(".ovm-AlternativeMarketHeader")
+        
+        for (let i = 0; i < elementos.length; i++) {
+            let elemento = elementos[i].innerText
+            if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Resultado da Prorrogação") {
+                console.log(elemento)
+                alert(elemento)
+            } else {
             console.log(elemento)
-            alert(elemento)
-        } else {
-            console.log(elemento)
+            }
         }
     }
+    
+    function loop() {
+        setTimeout(() => {
+            penalti()
+            loop()
+        }, 10000)
+    }
+      
 }
 
 init()
