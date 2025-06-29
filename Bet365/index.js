63e563a479d12022469764eec69c0366e7db0d91
filipemilk <filipeleche@gmail.com>
2025-06-29 15:38:33 -0300

    contador

--- Bet365/index.js
+++ Bet365/index.js
@@ -1,10 +1,16 @@
 "use strict"
 
 function init(){
+
+    var contador = 1
     
     function penalti() {
         const elementos = document.querySelectorAll(".ovm-AlternativeMarketHeader")
         
+        console.log(`${contador}ª - Verificação`)
+        console.log("-----------------------------------")
+        contador++
+
         for (let i = 0; i < elementos.length; i++) {
             let elemento = elementos[i].innerText
             if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Resultado da Prorrogação") {
