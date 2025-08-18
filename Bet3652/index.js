4cc2fd4bd9137aa176c0d898b1c0a35822c50f39
filipemilk <filipeleche@gmail.com>
2025-08-18 18:56:15 -0300

    Bet2

---
+++ Bet3652/index.js
@@ -0,0 +1,36 @@
+"use strict"
+
+function init(){
+
+    const audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3')
+    var contador = 1
+    
+    function penalti() {
+        const elementos = document.querySelectorAll(".ovm-AlternativeMarketHeader")
+        
+        console.log(`${contador}ª - Verificação`)
+        console.log("-----------------------------------")
+        contador++
+
+        for (let i = 0; i < elementos.length; i++) {
+            let elemento = elementos[i].innerText
+            if(elemento == "Para Ganhar nos Pênaltis" || elemento == "Resultado da Prorrogação") {
+                console.log(elemento)
+                audio.play()
+            } else {
+            console.log(elemento)
+            }
+        }
+        console.log("-----------------------------------")
+    }
+    
+    (function loop() {
+        setTimeout(() => {
+            penalti()
+            loop()
+        }, 30000)
+    }())
+      
+}
+
+init()
